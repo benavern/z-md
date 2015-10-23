27c0ee96d191983722662bb4e77ec123fcdc64fa
Yann Bertrand <yann@bertrand.com>
2015-10-23 20:58:10 +0200

    ctrl+s save the example file

---
+++ EXAMPLE.md
@@ -0,0 +1,3 @@
+# Hello World
+
+Welcome to this **fabulous** *Markdown editor*!
--- _js-files/app.js
+++ _js-files/app.js
@@ -1,16 +1,33 @@
 // Import Catdown
 var Catdown = require('catdown');
+var fs = require('fs');
+
+var currentFile = './EXAMPLE.md';
 
 var editor = document.getElementById('editor');
 var preview = document.getElementById('preview');
 
 // Create an editor
 var catdown = new Catdown({
-    textarea: editor,
-    preview: preview
+  textarea: editor,
+  preview: preview,
 });
 
-var fs = require('fs');
-fs.readFile('./README.md', 'utf8', function (error, data) {
+catdown.setKeys({
+  'Ctrl-I': function () {
+    this._surroundSelection('*');
+  },
+  'Ctrl-B': function () {
+    this._surroundSelection('**');
+  },
+  'Ctrl-S': function () {
+    fs.writeFile(currentFile, this.value(), function (error) {
+      // ToDo: handle error
+    });
+  }
+});
+
+fs.readFile(currentFile, 'utf8', function (error, data) {
+  // Update data
   catdown.set(data);
 });
